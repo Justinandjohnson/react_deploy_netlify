5c9407275575ee604e3c012c76f09c2f7f020c54
Justin <justinandjohnson@gmail.com>
2022-11-22 15:36:32 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+//import backgroundvideo from "./components/background";
+
+//import gify from '..xx/src/assets/gify.gif';
+
+
+import myGif from '../src/assets/gify.gif';
+import SearchBar from "./components/weather";
+
+const App = () => {
+  return (
+  <div>
+    
+      
+      <SearchBar/>
+      
+    </div>
+  );
+};
+
+export default App;
